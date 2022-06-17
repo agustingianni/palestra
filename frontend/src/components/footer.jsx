c770d8c0c1e9319cd37c3bd7ffe51ff85f9a487c
Agustin Gianni <agustingianni@gmail.com>
2022-06-17 12:39:43 +0200

    Frontend: remove unused components

--- frontend/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className='footer'>
-            <p><a href="mailto:agustingianni@gmail.com">Contact</a></p>
-        </footer>
-    )
-}
-
-export default Footer
